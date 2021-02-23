2ab14005b14866234f8a7d1d107f801b181529dd
Kasia Laniecka <kl@parkshare.dk>
2021-02-23 23:03:23 +0100

    background and fonts

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div className="home">
-            home
-            <Link to="/">
-                go
-            </Link>
+            <div className="background">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className="search">
+                <h1>Sunset Today</h1>
+                <h2>We all love sunsets, so grab a friend, a camera and don't miss the moment! Find out how much time is left until sunset today at your location</h2>
+            </div>
+
         </div>
     );
 }

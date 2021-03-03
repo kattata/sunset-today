a8837bb265eb7b62dda8c9f548e4b78fe89ab713
Kasia Laniecka <kl@parkshare.dk>
2021-03-03 22:02:06 +0100

    home exit animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 import { motion } from 'framer-motion';
 
-const transition = { ease: "easeOut", duration: 2 };
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
 const Home = ({ convertCity }) => {
     const history = useHistory();
@@ -26,26 +26,35 @@ const Home = ({ convertCity }) => {
     return (
         <div className="home">
             <div className="background">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+                <motion.span initial={{ x: "-50%" }} exit={{ height: "300vh", width: "300vh", opacity: 1, y: "-125vh" }} transition={transition, { delay: 0.9, duration: 1.2 }}></motion.span>
+                <motion.span></motion.span>
+                <motion.span></motion.span>
+                <motion.span></motion.span>
+                <motion.span></motion.span>
                 <div className="search">
                     <motion.h1
                         key="h1"
                         exit={{ opacity: 0 }}
-                        // whileHover={{ opacity: 0 }}
                         transition={transition}
-                    >Sunset Today</motion.h1>
-                    <h2>We all love sunsets, so grab a friend, a camera and don't miss the moment! Find out how much time is left until the sunset today at your location</h2>
-                    <form onSubmit={handleSubmit}>
+                    >Sunset Today
+                    </motion.h1>
+                    <motion.h2
+                        key="h1"
+                        exit={{ opacity: 0 }}
+                        transition={transition, { delay: 0.3 }}
+                    >We all love sunsets, so grab a friend, a camera and don't miss the moment! Find out how much time is left until the sunset today at your location
+                        </motion.h2>
+                    <motion.form
+                        key="h1"
+                        exit={{ opacity: 0 }}
+                        transition={transition, { delay: 0.6 }}
+                        onSubmit={handleSubmit}>
                         <input type="text" placeholder="Type in city and press enter" value={city} onChange={handleChange} />
-                    </form>
+                    </motion.form>
                 </div>
 
             </div>
-        </div>
+        </div >
     );
 }
 
